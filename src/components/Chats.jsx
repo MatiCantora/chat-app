b09329046bae2d26115667369989feeb139201ae
MatiCantora <cantoramatias@gmail.com>
2022-10-18 13:57:24 -0300

    Chat layout

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,5 +1,16 @@
 import React from "react"
+import user from "../img/user.png"
 
 export const Chats = () => {
-	return <div>Chats</div>
+	return (
+		<div className="chats">
+			<div className="user-chat">
+				<img src={user} alt="" />
+				<div className="user-chat-info">
+					<span>Josefina</span>
+					<p>Hola</p>
+				</div>
+			</div>
+		</div>
+	)
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,18 @@
 import React from "react"
+import user from "../img/user.png"
 
 export const Search = () => {
-	return <div>Search</div>
+	return (
+		<div className="search">
+			<div className="search-form">
+				<input type="text" name="" id="" placeholder="Buscar contacto" />
+			</div>
+			<div className="user-chat">
+				<img src={user} alt="" />
+				<div className="user-chat-info">
+					<span>Josefina</span>
+				</div>
+			</div>
+		</div>
+	)
 }
