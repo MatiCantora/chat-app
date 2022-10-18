57399ac5e655d4474865308cbed18f6adf0f2f73
MatiCantora <cantoramatias@gmail.com>
2022-10-18 14:45:45 -0300

    Messages layout

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,19 @@
 import React from "react"
+import dots from "../img/dots.png"
+import { Messages } from "./Messages"
+import { Input } from "./Input"
 
 export const Chat = () => {
-	return <div className="chat"></div>
+	return (
+		<div className="chat">
+			<div className="chat-info">
+				<span>Josefina</span>
+				<div className="chat-icons">
+					<img src={dots} alt="" />
+				</div>
+			</div>
+			<Messages />
+			<Input />
+		</div>
+	)
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,20 @@
 import React from "react"
+import avatar from "../img/avatar.png"
+import send from "../img/send.png"
 
 export const Input = () => {
-	return <div>Input</div>
+	return (
+		<div className="input">
+			<input type="text" name="" id="" placeholder="Di algo..." />
+			<div className="send">
+				<input style={{ display: "none" }} type="file" name="" id="file" />
+				<label htmlFor="file">
+					<img src={avatar} alt="" />
+				</label>
+				<button>
+					<img src={send} alt="" />
+				</button>
+			</div>
+		</div>
+	)
 }
